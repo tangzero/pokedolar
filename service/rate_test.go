a64720b8879204b3cfc57c11f3087bd9a0d289b2
Jairinho <jairo.silva@thoughtworks.com>
2020-08-18 18:27:42 -0400

    mock http da função Rate

--- service/rate_test.go
+++ service/rate_test.go
@@ -1,24 +1,29 @@
 package service_test
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"pokedolar/service"
 	"testing"
 )
 
 
 func TestRatings(t *testing.T) {
-	cases := []struct{
-		from string
-		to string
-		expected float64
-	}{
-		{from: "USD", to: "BRL", expected: 5.44},
-		{from: "USD", to: "EUR", expected: 0.83},
+	t.Cleanup(func() {
+		http.DefaultClient.Transport = nil
+	})
+
+	cases := []testCase{
+		{from: "USD", to: "BRL", expected: 5.39},
+		{from: "USD", to: "EUR", expected: 0.76},
 	}
 
 	for _, c := range cases {
 		t.Run(fmt.Sprintf("from_%s_to_%s", c.from, c.to), func(t *testing.T) {
+			http.DefaultClient.Transport = &mockHTTP{c}
+
 			rate, err := service.Rate(c.from, c.to)
 
 			if err != nil {
@@ -32,4 +37,23 @@ func TestRatings(t *testing.T) {
 	}
 }
 
+type testCase struct{
+	from string
+	to string
+	expected float64
+}
 
+type mockHTTP struct {
+	test testCase
+}
+
+func (m *mockHTTP) RoundTrip(r *http.Request) (*http.Response, error) {
+	query := r.URL.Query()
+	if query.Get("base") == m.test.from && query.Get("symbols") == m.test.to {
+		rec := httptest.NewRecorder()
+		rec.Code = 200
+		fmt.Fprintf(rec.Body, "{ \"rates\": { \"%s\": %f } }", m.test.to, m.test.expected)
+		return rec.Result(), nil
+	}
+	return nil, errors.New("not found")
+}
